e52d753224ce7f1d9223e228d64be503b080e568
francis <nkamande@kabarak.ac.ke>
2022-06-03 11:50:52 +0300

    Test class for the Math class in the source folder. @Test annotation to define that the test method.

---
+++ test/com/in28Minutes/junit/MyMathTest.java
@@ -0,0 +1,12 @@
+package com.in28Minutes.junit;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MyMathTest {
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+}
