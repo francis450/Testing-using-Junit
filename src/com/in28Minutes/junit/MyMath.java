8f984f994e2ab9f2eedeed4f774e013bb2340355
francis <nkamande@kabarak.ac.ke>
2022-06-03 11:49:02 +0300

    A simple math class without any annotations to be tested

---
+++ src/com/in28Minutes/junit/MyMath.java
@@ -0,0 +1,12 @@
+package com.in28Minutes.junit;
+
+public class MyMath {
+
+    int sum(int [] numbes){
+        int sum = 0;
+        for(int i : numbers){
+            sum += i;
+        }
+        return sum;
+    }
+}
